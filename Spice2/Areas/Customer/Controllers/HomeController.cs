83afb65dacb1dfc18380e8f0c0023c2d4a975770
Elena Granados <EGranados@hasc.org>
2020-09-11 17:50:21 -0700

    check for user type

--- Spice2/Areas/Customer/Controllers/HomeController.cs
+++ Spice2/Areas/Customer/Controllers/HomeController.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using Spice2.Data;
 using Spice2.Models;
 using Spice2.Models.ViewModels;
+using Spice2.Utility;
 
 namespace Spice2.Controllers
 {
@@ -22,17 +25,96 @@ namespace Spice2.Controllers
             _db = db;
         }
 
+
         public async Task<IActionResult> Index()
         {
             IndexViewModel IndexVM = new IndexViewModel()
             {
                 MenuItem = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).ToListAsync(),
                 Category = await _db.Category.ToListAsync(),
-                Coupon = await _db.Coupon.Where(c => c.isActive == true).ToListAsync()
+                Coupon = await _db.Coupon.Where(c => c.IsActive == true).ToListAsync()
+
             };
+
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (claim != null)
+            {
+                var cnt = _db.ShoppingCart.Where(u => u.ApplicationUserId == claim.Value).ToList().Count;
+                HttpContext.Session.SetInt32(SD.ssShoppingCartCount, cnt);
+            }
+
 
             return View(IndexVM);
         }
+
+        [Authorize]
+        public async Task<IActionResult> Details(int id)
+        {
+            var menuItemFromDb = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).Where(m => m.Id == id).FirstOrDefaultAsync();
+
+            ShoppingCart cartObj = new ShoppingCart()
+            {
+                MenuItem = menuItemFromDb,
+                MenuItemId = menuItemFromDb.Id
+            };
+
+            return View(cartObj);
+        }
+
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Details(ShoppingCart CartObject)
+        {
+            CartObject.Id = 0;
+            if (ModelState.IsValid)
+            {
+                var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+                var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+                CartObject.ApplicationUserId = claim.Value;
+
+                ShoppingCart cartFromDb = await _db.ShoppingCart.Where(c => c.ApplicationUserId == CartObject.ApplicationUserId
+                                                && c.MenuItemId == CartObject.MenuItemId).FirstOrDefaultAsync();
+
+                if (cartFromDb == null)
+                {
+                    await _db.ShoppingCart.AddAsync(CartObject);
+                }
+                else
+                {
+                    cartFromDb.Count = cartFromDb.Count + CartObject.Count;
+                }
+                await _db.SaveChangesAsync();
+
+                var count = _db.ShoppingCart.Where(c => c.ApplicationUserId == CartObject.ApplicationUserId).ToList().Count();
+                //"SD.ssShoppingCartCount" is the name we give to the sessions
+                //var _count = count;
+
+                HttpContext.Session.SetInt32(SD.ssShoppingCartCount, count);
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+
+                var menuItemFromDb = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).Where(m => m.Id == CartObject.MenuItemId).FirstOrDefaultAsync();
+
+                ShoppingCart cartObj = new ShoppingCart()
+                {
+                    MenuItem = menuItemFromDb,
+                    MenuItemId = menuItemFromDb.Id
+                };
+
+                return View(cartObj);
+            }
+        }
+
+
+
+
 
         public IActionResult Privacy()
         {
