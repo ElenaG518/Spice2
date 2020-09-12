5884ef7e31a5076aa9a8b60a9d06e8c56d2a3a01
Elena Granados <EGranados@hasc.org>
2020-09-11 17:56:44 -0700

    add ssCartCount

--- Spice2/Utility/SD.cs
+++ Spice2/Utility/SD.cs
@@ -8,9 +8,13 @@ namespace Spice2.Utility
     public static class SD
     {
         public const string DefaultFoodImage = "default_food.png";
+
+
         public const string ManagerUser = "Manager";
         public const string KitchenUser = "Kitchen";
         public const string FrontDeskUser = "FrontDesk";
         public const string CustomerEndUser = "Customer";
+
+        public const string ssShoppingCartCount = "ssCartCount";
     }
 }
